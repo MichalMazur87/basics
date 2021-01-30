a451f2f184bfc82edea479002843a490e1042e8b
Michal Mazur <michalmazur87@gmail.com>
2021-01-30 13:25:49 +0100

    1

--- toto.py
+++ toto.py
@@ -4,4 +4,9 @@ liczba = random.randint(1,10)
 #print("Wylosowana liczba:",liczba)
 
 odp = input("Jaką liczbę od 1 do 10 mam na myśli?")
-print("Podałeś liczbę:",odp)
+#print("Podałeś liczbę:",odp)
+
+if liczba == int(odp):
+    print("Zgadłeś! Dostajesz długopis!")
+else:
+    print("Nie zgadłeś. Spróbuj jeszcze raz.")
