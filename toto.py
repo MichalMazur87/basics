56540d0ca8272d2c288ffca2bbf43deaafce81cb
Michal Mazur <michalmazur87@gmail.com>
2021-01-30 14:06:18 +0100

    dodane 3 szanse

--- toto.py
+++ toto.py
@@ -3,10 +3,14 @@ import random
 liczba = random.randint(1,10)
 #print("Wylosowana liczba:",liczba)
 
-odp = input("Jaką liczbę od 1 do 10 mam na myśli?")
-#print("Podałeś liczbę:",odp)
 
-if liczba == int(odp):
-    print("Zgadłeś! Dostajesz długopis!")
-else:
-    print("Nie zgadłeś. Spróbuj jeszcze raz.")
+for i in range (3):
+    odp = input("Jaką liczbę od 1 do 10 mam na myśli?")
+    print("Podałeś liczbę:",odp)
+   
+    if liczba == int(odp):
+        print("Zgadłeś! Dostajesz długopis!")
+        break
+    else:
+        print("Nie zgadłeś. Spróbuj jeszcze raz.")
+        print()
