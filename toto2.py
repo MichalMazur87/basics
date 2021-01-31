0786777c3bf87cce6e788ff5307c43fb526060e4
Michal Mazur <michalmazur87@gmail.com>
2021-01-31 21:11:39 +0100

    podanie zmiennych

---
+++ toto2.py
@@ -0,0 +1,8 @@
+ileliczb = input("Ile liczb chcesz odgadnąć?")
+ileliczb = int(ileliczb)
+print (ileliczb)
+maksliczba = input("Jaka jest maksymalna losowana liczba?")
+maksliczba = int(maksliczba)
+print (maksliczba)
+#print ("Wytypuj ",ileliczb," z ",maksliczba," liczb: ")
+print("Wytypuj %d z %d liczb: " % (ileliczb,maksliczba))
