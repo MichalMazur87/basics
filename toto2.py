04795b284f7f02bdf68ac34628c7edca47f70759
Michal Mazur <michalmazur87@gmail.com>
2021-01-31 21:58:25 +0100

    dodane losowanie

--- toto2.py
+++ toto2.py
@@ -1,9 +1,23 @@
 import random
 
-ileliczb = input("Ile liczb chcesz odgadnąć?")
-ileliczb = int(ileliczb)
-print (ileliczb)
+ileliczb = int(input("Ile liczb chcesz odgadnąć?"))
+#print (ileliczb)
 maksliczba = int(input("Jaka jest maksymalna losowana liczba?"))
-print (maksliczba)
+#print (maksliczba)
 #print ("Wytypuj ",ileliczb," z ",maksliczba," liczb: ")
 print("Wytypuj %d z %d liczb: " % (ileliczb,maksliczba)) #to samo z innym zapisem, %s string, %d dziesiętna, %f float
+
+i=0
+wylosowane=[]
+while i < ileliczb:
+    liczba = random.randint(1,maksliczba)
+    print(liczba)
+    if wylosowane.count(liczba)==0:
+        wylosowane.append(liczba)
+        i+=1
+
+
+
+
+print("Wylosowane liczby: ", wylosowane)
+    
