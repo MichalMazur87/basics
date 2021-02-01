479dbee53d7c9a67f1846bd7f8eca95329785a30
Michal Mazur <michalmazur87@gmail.com>
2021-02-01 21:17:38 +0100

    wymiana listy na zbiór

--- toto2.py
+++ toto2.py
@@ -7,17 +7,16 @@ maksliczba = int(input("Jaka jest maksymalna losowana liczba?"))
 #print ("Wytypuj ",ileliczb," z ",maksliczba," liczb: ")
 print("Wytypuj %d z %d liczb: " % (ileliczb,maksliczba)) #to samo z innym zapisem, %s string, %d dziesiętna, %f float
 
+typy=set()
 i=0
-wylosowane=[]
 while i < ileliczb:
-    liczba = random.randint(1,maksliczba)
-    print(liczba)
-    if wylosowane.count(liczba)==0:
-        wylosowane.append(liczba)
+    typ = input("podaj liczbę %s: " % (i+1))
+    if typ not in typy:
+        typy.add(typ)
         i+=1
 
 
 
 
-print("Wylosowane liczby: ", wylosowane)
+print("Wylosowane liczby: ", typy)
     
