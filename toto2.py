14eff2421da792aace633eb182561dbb29586165
Michal Mazur <michalmazur87@gmail.com>
2021-02-02 17:15:35 +0100

    naprawiona logika

--- toto2.py
+++ toto2.py
@@ -15,14 +15,23 @@ while i < ileliczb:
         i+=1
 
 #pobieramy typy od użytkownika
-print ("Wytypuj ",ileliczb," z ",maksliczba," liczb: ")
 print("Wytypuj %d z %d liczb: " % (ileliczb,maksliczba)) #to samo z innym zapisem, %s string, %d dziesiętna, %f float
 typy=set()
 i=0
 while i < ileliczb:
-    typ = input("podaj liczbę %s: " % (i+1))
+    typ = int(input("podaj liczbę %s: " % (i+1)))
     if typ not in typy:
         typy.add(typ)
         i+=1
 
-print("Wylosowane liczby: ", typy)
+
+print("\nWylosowane liczby: ", liczby)
+
+
+#prezentacja wyników
+trafione = set(liczby) & typy
+if trafione:
+    print("Ilość trafień: %s z %s" % ((len(trafione)),ileliczb))
+    print("Trafione liczby: ", trafione)
+else:
+    print("Brak trafień. Spróbuj jeszcze raz!")
