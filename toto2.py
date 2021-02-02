fbf63e009857f9174498393cd5e6a242d504f5ec
Michal Mazur <michalmazur87@gmail.com>
2021-02-02 13:13:12 +0100

    przyrócenie losowania

--- toto2.py
+++ toto2.py
@@ -4,9 +4,19 @@ ileliczb = int(input("Ile liczb chcesz odgadnąć?"))
 #print (ileliczb)
 maksliczba = int(input("Jaka jest maksymalna losowana liczba?"))
 #print (maksliczba)
-#print ("Wytypuj ",ileliczb," z ",maksliczba," liczb: ")
-print("Wytypuj %d z %d liczb: " % (ileliczb,maksliczba)) #to samo z innym zapisem, %s string, %d dziesiętna, %f float
 
+#losujemy liczby
+liczby=[]
+i=0
+while i < ileliczb:
+    liczba=random.randint(1,maksliczba)
+    if liczby.count(liczba) == 0:
+        liczby.append(liczba)
+        i+=1
+
+#pobieramy typy od użytkownika
+print ("Wytypuj ",ileliczb," z ",maksliczba," liczb: ")
+print("Wytypuj %d z %d liczb: " % (ileliczb,maksliczba)) #to samo z innym zapisem, %s string, %d dziesiętna, %f float
 typy=set()
 i=0
 while i < ileliczb:
@@ -15,8 +25,4 @@ while i < ileliczb:
         typy.add(typ)
         i+=1
 
-
-
-
 print("Wylosowane liczby: ", typy)
-    
